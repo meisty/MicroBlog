6715cc7566122521e54ed24b91c9874ff7cc7470
meisty <shaundixon666@gmail.com>
2018-10-04 21:19:37 +0100

    Completed initial work on web forms and implementing login system

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,12 +8,22 @@ def index():
     user = {'username': 'Shaun'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'username': 'Shaun'},
+            'body': 'Today was titled "Fun Day" as there was no syllabus to learn.  In reality it ended up being an hour long session of calisthetics.  After my body could barely function.  It was great. '
         },
         {
             'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
+
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
